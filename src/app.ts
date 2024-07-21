4085ac81040afbd2e236c212f5a877cb394a4fc7
William <williampo78@gmail.com>
2024-07-21 15:55:01 +0800

    connect to mongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,29 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
 import billRoutes from './routes';
 
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT;
 
 //middleware
 
 app.use(express.json());
 app.use((req, res, next) => {
     console.log(123465);
-    next()
-})
-
-app.use('/api/bills', billRoutes)
-
-app.listen(port, () => {
-    console.log(`server is listening on ${port} !!!`);
+    next();
 });
+
+app.use('/api/bills', billRoutes);
+
+mongoose
+    .connect(process.env.MONGODB_URI!)
+    .then(() => {
+        console.log('MondoDB connected...');
+
+        app.listen(port, () => {
+            console.log(`server is listening on ${port} !!!`);
+        });
+    })
+    .catch((err) => console.error(err));
 
